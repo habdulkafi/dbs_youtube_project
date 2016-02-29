8045fbd5312ce61692f0be415d37d9edac4f8f0d
Husam <hsa2136@columbia.edu>
2016-02-29 16:43:17 -0500

    stupid bug

--- test.py
+++ test.py
@@ -44,7 +44,7 @@ for channel in results["items"]:
 	channeldesc = chresults["items"][0]["snippet"]["description"].replace("'","")
 	channelviewcount = chresults["items"][0]["statistics"]["viewCount"]
 	# q = "INSERT INTO channel (c_id, c_title, c_description, c_view_count, c_sub_count) VALUES ('" + channelId + "','" + channeltitle + "','" + channeldesc + "'," + str(channelviewcount) + "," + str(channelsubcount) + ")" 
-	q = "INSERT INTO channel (c_id, c_title, c_description, c_view_count, c_sub_count) SELECT '{0}' ,'{1}','{2}',{3},{4} FROM channel WHERE NOT EXISTS (SELECT 1 FROM channel WHERE c_id = '{0}')".format(channelId,channeltitle,channeldesc,str(channelviewcount),str(channelsubcount)) 
+	q = "INSERT INTO channel (c_id, c_title, c_description, c_view_count, c_sub_count) SELECT '{0}' ,'{1}','{2}',{3},{4} WHERE NOT EXISTS (SELECT 1 FROM channel WHERE c_id = '{0}')".format(channelId,channeltitle,channeldesc,str(channelviewcount),str(channelsubcount)) 
 	cur.execute(q)
 	thumdict = chresults["items"][0]["snippet"]["thumbnails"]
 	for size in thumdict:
@@ -53,6 +53,7 @@ for channel in results["items"]:
 		cur.execute(q)
 		q = "INSERT INTO has_thumb_2 (t_url,c_id) SELECT '{0}','{1}' WHERE NOT EXISTS (SELECT 1 FROM has_thumb_2 WHERE t_url = '{0}')".format(thumurl,channelId)
 		cur.execute(q)
+		print size, channelId
 
 
 # df = pandas.read_sql("SELECT * FROM video",con=engine)
